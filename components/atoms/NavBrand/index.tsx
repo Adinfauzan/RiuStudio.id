33c9d20500d2a12748dbc45bd956f2ce5d894bce
Riu <echo.adinfauzan@gmail.com>
2024-07-28 00:08:15 +0700

    Update index.tsx

--- components/atoms/NavBrand/index.tsx
+++ components/atoms/NavBrand/index.tsx
@@ -14,7 +14,7 @@ const NavBrand = () => {
       >
         <Link href={'/'} passHref>
           <div className="w-fit h-fit">
-            <Image src={'/images/logo/RiuStudio.svg'} color= 'white' width='135px' height={30} alt='company brand' loading='lazy' />
+            <Image src={'/images/logo/RiuStudio.svg'} width='100px' height='100px' alt='RiuStudio Company' loading='lazy' />
           </div>
         </Link>
       </div>
